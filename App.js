8e0283e9690acc7c848e710c5228ef2595d86ee9
Gary Fagan <garyfagan1987@googlemail.com>
2020-06-11 08:16:14 +0100

    adding push notifications

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
-import React, { useState } from 'react';
-import { Alert, FlatList } from 'react-native';
+import React from 'react';
+import { Alert, FlatList, Platform, Vibration } from 'react-native';
+import { Notifications } from 'expo';
+import * as Permissions from 'expo-permissions';
+import Constants from 'expo-constants';
 
 import Container from './components/Container';
 import Empty from './components/Empty';
@@ -8,53 +11,137 @@ import List from './components/List';
 import Main from './components/Main';
 import Title from './components/Title';
 
-export default function App() {
-  const [todos, setTodos] = useState([]);
-  const [todo, setTodo] = useState('');
+export default class App extends React.Component {
+  state = {
+    expoPushToken: '',
+    notification: {},
+    todos: [],
+    todo: '',
+  };
 
-  const handleSubmit = () => {
+  handleSubmit = () => {
+    const { todo, todos } = this.state;
     if(todo.length > 3) {
-      setTodos((prevTodos) => {
-        return [
-          ...prevTodos,
-          {
-            checked: false,
-            id: prevTodos.length + 1,
-            name: todo,
-          },
-        ];
-      });
-      setTodo('');
+      this.setTodos({checked: false, id: todos.length + 1, name: todo });
+      this.setTodo('');
     } else {
       Alert.alert('Woops', 'Todo must be over three characters long', [{ text: 'Understood' }])
     }
   }
 
-  const handleChecked = (id) => {
-    setTodos((prevTodos) => prevTodos.filter(todo => todo.id != id));
+  handleChecked = (id) => {
+    this.sendPushNotification()
+    this.removeTodo(id);
   }
 
-  return (
-    <Container>
-      <Title>Todo ({todos.length})</Title>
-      <Main>
-        <Form
-          handleSubmit={handleSubmit}
-          onChangeText={text => setTodo(text)}
-          value={todo}
-        />
-        {todos.length < 1 && <Empty>No todos, enjoy the rest of your day</Empty>}
-        <FlatList
-          data={todos}
-          keyExtractor={item => item.id.toString()}
-          renderItem={({ item }) => (
-            <List
-              onChange={() => handleChecked(item.id)}
-              item={item}
-            />
-          )}
-        />
-      </Main>
-    </Container>
-  );
+  registerForPushNotificationsAsync = async () => {
+    if (Constants.isDevice) {
+      const { status: existingStatus } = await Permissions.getAsync(Permissions.NOTIFICATIONS);
+      let finalStatus = existingStatus;
+      if (existingStatus !== 'granted') {
+        const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+        finalStatus = status;
+      }
+      if (finalStatus !== 'granted') {
+        alert('Failed to get push token for push notification!');
+        return;
+      }
+      token = await Notifications.getExpoPushTokenAsync();
+      this.setState({ expoPushToken: token });
+    } else {
+      alert('Must use physical device for Push Notifications');
+    }
+
+    if (Platform.OS === 'android') {
+      Notifications.createChannelAndroidAsync('default', {
+        name: 'default',
+        sound: true,
+        priority: 'max',
+        vibrate: [0, 250, 250, 250],
+      });
+    }
+  };
+
+  componentDidMount() {
+    this.registerForPushNotificationsAsync();
+    this._notificationSubscription = Notifications.addListener(this._handleNotification);
+  };
+
+  _handleNotification = notification => {
+    Vibration.vibrate();
+    console.log(notification);
+    this.setState({ notification: notification });
+  };
+
+  sendPushNotification = async () => {
+    const message = {
+      to: this.state.expoPushToken,
+      sound: 'default',
+      title: 'Todo Completed',
+      body: 'Awesome you completed a Todo!',
+      data: { data: 'goes here' },
+      _displayInForeground: true,
+    };
+
+    await fetch('https://exp.host/--/api/v2/push/send', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Accept-encoding': 'gzip, deflate',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(message),
+    });
+  };
+
+  setTodo(text) {
+    this.setState(() => {
+      return {todo: text};
+    })
+  }
+
+  setTodos(todo) {
+    this.setState((prevState) => {
+      return {
+        todos: [
+          ...prevState.todos,
+          todo,
+        ]
+      };
+    })
+  }
+
+  removeTodo(id) {
+    const todos = this.state.todos.filter(todo => todo.id != id);
+    this.setState(() => {
+      return { todos }
+    });
+  }
+
+  render() {
+    const { todo, todos } = this.state;
+    return (
+      <Container>
+        <Title>Todo</Title>
+        <Main>
+          <Form
+            handleSubmit={this.handleSubmit}
+            onChangeText={text => this.setTodo(text)}
+            value={todo}
+          />
+          {todos.length < 1 && <Empty>No todos, enjoy the rest of your day</Empty>}
+          <FlatList
+            data={todos}
+            keyExtractor={item => item.id.toString()}
+            renderItem={({ item }) => (
+              <List
+                onChange={() => this.handleChecked(item.id)}
+                item={item}
+              />
+            )}
+          />
+        </Main>
+      </Container>
+    );
+  }
 }
